746ca959ea9e5d2b0fac9c3c81a991611d084c3b
Tim Roush <troush@mail.greenriver.edu>
2017-05-03 16:32:17 -0500

    Corrected Fat Free Framework Setup and initial route.  Added placeholder home page.

--- index.php
+++ index.php
@@ -6,6 +6,8 @@
     $f3 = Base::instance();
     $f3->set('DEBUG', 3);
     
-    $f3->route('GET /', function($f3) {
-        echo 'TEST'; //\Template::instance()->render('view/home.html');
-    });
+    $f3->route('GET /', function($f3) {                     // Default Route
+        echo \Template::instance()->render('view/home.html');
+      });
+    
+    $f3->run();
